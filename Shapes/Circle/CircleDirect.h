9216546b3e91c4b8929c758bc69dcebb0dd9bfc9
yousef-showman <123179662+yousef-showman@users.noreply.github.com>
2025-06-02 17:29:11 +0200

    اخر كلام

--- Shapes/Circle/CircleDirect.h
+++ Shapes/Circle/CircleDirect.h
@@ -4,6 +4,18 @@
 #include <windows.h>
 #include <cmath>
 #include <vector>
+#include "../../Classes/Point.h"
+#include "eightPoint.h"
+
+void drawCircleDirect(HDC hdc, std::vector<Point> points, COLORREF color) {
+    if(points.size() < 2) return;
+    int xc = points[0].x;
+    int yc = points[0].y;
+    int dx = points[1].x - xc;
+    int dy = points[1].y - yc;
+    int R = static_cast<int>(sqrt(dx*dx + dy*dy));
+    
+    int x = 0;
     int y = R;
     int R2 = R * R;
     
